29e02a0f58f55dd400ae856b2502e1c291d2a5f3
Flash3r01 <tim-johmann@web.de>
2018-10-19 16:00:39 +0200

    Removed unnecessary files.

--- GuiTest/src/GuiTaschenrechner/Controller.java
+++
@@ -1,8 +0,0 @@
-package GuiTaschenrechner;
-
-public class Controller {
-    public static void main(String[] args) {
-        adfasdf.main(args);
-
-    }
-}
--- Programiervorkurs/src/SchiffeVersenken/Controller.java
+++
@@ -1,4 +0,0 @@
-package SchiffeVersenken;
-
-public class Controller {
-}
